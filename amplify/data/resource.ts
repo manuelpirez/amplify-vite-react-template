728699d885212a40ff0a1f8f3bbc68812d240fd3
manuel.pirez <manuel.pirez@pslgroup.com>
2024-12-13 17:27:27 -0600

    Added amplify connected form components and figma

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,4 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend'
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -7,22 +7,24 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a.model({
-    content: a.string(),
-  }).authorization(allow => [allow.owner()]),
+  Todo: a
+    .model({
+      content: a.string()
+    })
+    .authorization(allow => [allow.owner()])
 })
 
-export type Schema = ClientSchema<typeof schema>;
+export type Schema = ClientSchema<typeof schema>
 
 export const data = defineData({
   schema,
   authorizationModes: {
     // This tells the data client in your app (generateClient())
     // to sign API requests with the user authentication token.
-    defaultAuthorizationMode: 'userPool',
+    defaultAuthorizationMode: 'userPool'
     // API Key is used for a.allow.public() rules
-  },
-});
+  }
+})
 
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Authenticator } from '@aws-amplify/ui-react';
-import App from "./App.tsx";
-import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
-import '@aws-amplify/ui-react/styles.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Authenticator, ThemeProvider } from '@aws-amplify/ui-react'
+import App from './App.tsx'
+import './index.css'
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
+import '@aws-amplify/ui-react/styles.css'
 
+Amplify.configure(outputs)
 
-Amplify.configure(outputs);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <Authenticator>
-            <App />
-        </Authenticator>
-    </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Authenticator>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Authenticator>
+  </React.StrictMode>
+)
