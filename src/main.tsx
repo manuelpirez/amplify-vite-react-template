915e8ddf9b125231de4cd3d35794da54dc58c368
manuel.pirez <manuel.pirez@pslgroup.com>
2024-12-17 22:59:06 -0600

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import { Authenticator, ThemeProvider } from '@aws-amplify/ui-react'
 import App from './App.tsx'
 import './index.css'
 import { Amplify } from 'aws-amplify'
+import '@aws-amplify/ui-react/styles.css' // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import outputs from '../amplify_outputs.json'
-import '@aws-amplify/ui-react/styles.css'
 
 Amplify.configure(outputs)
 
